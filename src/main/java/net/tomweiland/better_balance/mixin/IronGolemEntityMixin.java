9800fb58d0f5e922362bb478f93b31796b855956
Tom Weiland <tomdweiland@gmail.com>
2025-05-24 12:23:29 -0700

    Make iron golems ignore persistent mobs
    Prevents persistent mobs (with a name or gear given by the player) from being used to lure iron golems to their death.

--- src/main/java/net/tomweiland/better_balance/mixin/IronGolemEntityMixin.java
+++ src/main/java/net/tomweiland/better_balance/mixin/IronGolemEntityMixin.java
@@ -3,7 +3,9 @@ package net.tomweiland.better_balance.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.Angerable;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.GolemEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.world.World;
@@ -19,5 +21,13 @@ public abstract class IronGolemEntityMixin extends GolemEntity implements Angera
     public boolean isPushedByFluids() {
         // Prevent water from being used to push iron golems around in farms
         return false;
+    }
+
+    @Override
+    public void setTarget(LivingEntity target) {
+        if (target instanceof MobEntity mob && mob.isPersistent()) {
+            return;
+        }
+        super.setTarget(target);
     }
 }
