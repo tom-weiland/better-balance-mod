72ab5dc65e6b3efd38f157be67c0041142b90b7b
Tom Weiland <tomdweiland@gmail.com>
2025-05-23 21:59:32 -0700

    Initial commit

---
+++ src/main/java/net/tomweiland/better_balance/BetterBalance.java
@@ -0,0 +1,16 @@
+package net.tomweiland.better_balance;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterBalance implements ModInitializer {
+	public static final String MOD_ID = "better_balance";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
