93f9ad2a5753fa40235350756a3454327005b684
Tom Weiland <tomdweiland@gmail.com>
2025-05-24 18:01:31 -0700

    Disallow sleeping

---
+++ src/main/java/net/tomweiland/better_balance/mixin/PlayerEntityMixin.java
@@ -0,0 +1,20 @@
+package net.tomweiland.better_balance.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.mojang.datafixers.util.Either;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Unit;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin {
+
+    @Inject(method = "trySleep", at = @At("HEAD"), cancellable = true)
+	private void onTrySleep(CallbackInfoReturnable<Either<PlayerEntity.SleepFailureReason, Unit>> cir) {
+        cir.setReturnValue(Either.left(PlayerEntity.SleepFailureReason.NOT_SAFE));
+	}
+}
